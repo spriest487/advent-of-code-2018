5153dbbe4c6d03cfae5e3bf4c9e0cf0426b660c5
spriest487 <spriest487@googlemail.com>
2018-12-02 20:20:58 +0000

    Day 2

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use {
-    std::{ 
-        env,
+    levenshtein::levenshtein,
+    std::{
         collections::HashSet,
+        env,
     },
 };
 
@@ -20,7 +21,7 @@ fn day_1() {
     let mut total = 0;
     let first_repeated = loop {
         total += values_cycle.next().unwrap();
-    
+
         if !prev_vals.insert(total) {
             break total;
         }
@@ -29,15 +30,67 @@ fn day_1() {
     println!("first repeated value: {}", first_repeated);
 }
 
+fn day_2() {
+    let input = include_str!("day_2.txt");
+    let ids: Vec<String> = input.lines()
+        .map(|line| line.chars().collect())
+        .collect();
+
+    let count_ids_with_n_repeated_chars = |n| {
+        ids.iter()
+            .filter(|line| {
+                line.chars()
+                    .map(|a| line.chars().filter(|b| a == *b).count())
+                    .any(|count| count == n)
+            })
+            .count()
+    };
+
+    let ids_2_repeated = count_ids_with_n_repeated_chars(2);
+    let ids_3_repeated = count_ids_with_n_repeated_chars(3);
+
+    let checksum = ids_2_repeated * ids_3_repeated;
+
+    println!("checksum: {}", checksum);
+
+    let mut similar_ids = None;
+    for id_a in ids.iter() {
+        for id_b in ids.iter().skip(1) {
+            if levenshtein(id_a, id_b) == 1 {
+                similar_ids = Some((id_a, id_b));
+                break;
+            }
+        }
+
+        if let Some((id_a, id_b)) = similar_ids {
+            println!("similar IDs:");
+            println!("  {}", id_a);
+            println!("  {}", id_b);
+
+            let mut common_letters = String::new();
+            for i in 0..id_a.len() {
+                let c = id_a.chars().skip(i).next().unwrap();
+                if c == id_b.chars().skip(i).next().unwrap() {
+                    common_letters.push(c);
+                }
+            }
+
+            println!("common letters: {}", common_letters);
+            break;
+        }
+    }
+}
+
 fn main() {
     match env::args().skip(1).next() {
         None => {
             println!("usage: aoc <day number>");
         }
-        
+
         Some(arg) => {
             match arg.as_str() {
                 "1" => day_1(),
+                "2" => day_2(),
                 _ => println!("day {} not available", arg),
             }
         }
